b9120c9853c950ed288705e05c7d794578daf6d8
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-09-24 10:49:10 +0900

    docker settings

--- bin/clear.sh
+++ bin/clear.sh
@@ -12,9 +12,9 @@ read -p "please input your choice: " choice
 
 cd $docker_dir
 if [ $choice -eq 1 ]; then
-    docker compose exec tetris bash -c "rm -f ./ranking.txt"
+    docker-compose exec tetris bash -c "rm -f ./ranking.txt"
 elif [ $choice -eq 2 ]; then
-    docker compose down
+    docker-compose down
 elif [ $choice -eq 3 ]; then
     docker-compose down --rmi all --volumes --remove-orphans
 else
