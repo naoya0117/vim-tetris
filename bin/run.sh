a4ab499a59d28363b1f8b9f814627b8be3c422e6
Naoya Matuhashi <naoyamatuhashi@NaoyanoMacBook-Pro.local>
2025-05-29 10:00:39 +0900

    update start up script

--- bin/run.sh
+++ bin/run.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-while getopts h OPT
-do
-    case $OPT in
-        h) echo "Usage: ./run.sh [name]"
-           echo "name: your name"
-           exit 0
-           ;;
-        \?) echo "Usage: ./run.sh [name]"
-            echo "name: your name"
-            exit 1
-            ;;
-    esac
+while getopts h OPT; do
+  case $OPT in
+  h)
+    echo "Usage: ./run.sh [name]"
+    echo "name: your name"
+    exit 0
+    ;;
+  \?)
+    echo "Usage: ./run.sh [name]"
+    echo "name: your name"
+    exit 1
+    ;;
+  esac
 done
-
-
 
 script_dir=$(dirname $0)
 cd ${script_dir}/..
 
-if [ -z ${1} ] ; then
-    echo "Please enter your name"
-    read -p "Name: " name
+if [ -z ${1} ]; then
+  echo "Please enter your name"
+  read -p "Name: " name
 else
-    name=${1}
-fi
-
-if [ -z "$(docker image ls | grep -e vim-tetris)" ] ; then
-    echo "vim-tetris image is not found"
-    echo "try to build vim-tetris image..."
-    docker compose build
+  name=${1}
 fi
 
 echo "start vim-tetris..."
